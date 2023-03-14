b348136fbb11d281812d940314a01e9ce21280ba
Maurice Mutua <94635472+AfroGit@users.noreply.github.com>
2023-03-14 21:58:35 +0000

    Had issues with digging deep enough to extract required info from objects in the array.Dot Notation was a useful tool in drilling down all the way to nested objects and their properties.

--- index.js
+++ index.js
@@ -172,24 +172,115 @@ lowercase letters, like this:
 
 */
 
-let users = [
-  {firstName : "Susan", lastName: "Steward"},
-  {firstName : "Daniel", lastName: "Longbottom"},
-  {firstName : "Jacob", lastName: "Black"}
-];
-let emailSnippet = '@smallcompany.com';
-let usersEmails = users.map(function(element){
-  return `${element.firstName.toLowerCase()}${element.lastName.toLowerCase()}${emailSnippet}`;
-});
-console.log(usersEmails);
+// let users = [
+//   {firstName : "Susan", lastName: "Steward"},
+//   {firstName : "Daniel", lastName: "Longbottom"},
+//   {firstName : "Jacob", lastName: "Black"}
+// ];
+// let emailSnippet = '@smallcompany.com';
+// let usersEmails = users.map(function(element){
+//   return `${element.firstName.toLowerCase()}${element.lastName.toLowerCase()}${emailSnippet}`;
+// });
+// console.log(usersEmails);
 // ---------------------------------------------------------------
 /* */ 
 // let arr = [2, 3, 5, 7]
 
-// arr.map(function(element, index, array){
-//     console.log(element);
-//     console.log(index);
-//     console.log(array);
-//     console.log(this);
-//     return element;
+// let metaArr = arr.map(function(element, index, array){
+//     // console.log(element);
+//     // console.log(index);
+//     // console.log(array);
+//     // console.log(this);
+//      return element;
 // }, 80);
+// console.log(metaArr);
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// -----------------------------------------------------------------
+
+let completeUserData = [
+  
+  {
+    firstName: "Susan",
+    lastName: "Steward",
+    pronouns: {
+      main: { capitalized: "She", lowerCase: "she" },
+      possessive: { capitalized: "Her", lowerCase: "her" }
+    },
+    additionalInfo: {
+      hometown: "Sidney",
+      favoriteColor: "green",
+      favoriteSeason: "summer",
+      favoriteFood: "pizza",
+      pet: { name: "Charlie", species: "dog" },
+      siblings: ["John", "Jerry", "Samantha"]
+    }
+  },
+
+  //start of object 2
+  {
+    firstName: "Daniel",
+    lastName: "Longbottom",
+    pronouns: {
+      main: { capitalized: "He", lowerCase: "he" },
+      possessive: { capitalized: "His", lowerCase: "his" }
+    },
+    additionalInfo: {
+      hometown: "London",
+      favoriteColor: "blue",
+      favoriteSeason: "spring",
+      favoriteFood: "tacos",
+      pet: { name: "Luna", species: "dog" },
+      siblings: ["Sarah", "Jenny", "Samantha"]
+    }
+  },
+
+  //start of object 3
+  {
+    firstName: "Jacob",
+    lastName: "Black",
+    pronouns: {
+      main: { capitalized: "He", lowerCase: "he" },
+      possessive: { capitalized: "His", lowerCase: "his" }
+    },
+    additionalInfo: {
+      hometown: "New York",
+      favoriteColor: "yellow",
+      favoriteSeason: "fall",
+      favoriteFood: "sushi",
+      pet: { name: "Milo", species: "cat" },
+      siblings: ["Ralph", "Ronald", "Reggie", "Regina", "Rachel"]
+    }
+  }
+];
+
+/* 
+
+        Challenge
+
+Use the map method to generate a new array of biographies 
+for our users from the previous tutorial. Each of the 
+biographies should look like this:
+
+["1. Susan Steward is from Sidney. She has a dog named Charlie. 
+ Her favorite color is green and her favorite food is pizza. Her 
+ siblings are John, Jerry, and Samantha.", "2. Daniel Longbottom is..." etc.]
+
+ It should use the following format, which you can use as a template: 
+
+"[index number + 1]. [person's full name] is from [hometown]. 
+ [Appropriate pronoun] has a [pet species] named [pet name]. 
+ [Appropriate pronoun] favorite color is [favorite color] and 
+ [appropriate pronoun] favorite food is [favorite food]. 
+ [Appropriate pronoun] siblings are [siblings]."
+
+Hint: for the arrays inside of the objects, it may be helpful to use the map method additional times, and it may be helpful to use the index and array arguments. The "this" argument is unnecessary. */
+
+let bioArray = completeUserData.map(function(element, index, array){
+  return [`${index}.${element.firstName} ${element.lastName} is from ${element.additionalInfo.hometown}. 
+ ${element.pronouns.main.capitalized} has a ${element.additionalInfo.pet.species} named ${element.additionalInfo.pet.name}. 
+ ${element.pronouns.possessive.capitalized} favorite color is ${element.additionalInfo.favoriteColor} and ${element.pronouns.possessive.lowerCase}
+ favorite food is ${element.additionalInfo.favoriteFood}. 
+ ${element.pronouns.possessive.capitalized} siblings are ${element.additionalInfo.siblings}.`]
+});
+console.log(bioArray);
